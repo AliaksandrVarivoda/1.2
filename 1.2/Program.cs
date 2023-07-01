9e9d1a838e35481a0d7dbfcccf038e774e90a044
Aliaksandr Varyvoda <dizi547@mail.ru>
2023-07-01 19:04:04 +0300

    Добавьте файлы проекта.

---
+++ 1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            
+            
+            
+
+            Console.WriteLine("Александр Варивода");
+            Console.WriteLine("Мне 23 года");
+            Console.WriteLine("Моя почта Dizi547@mail.ru");
+            Console.WriteLine("Баллы по программированию 1.5 ");
+            Console.WriteLine("Баллы по матеше 3");
+            Console.WriteLine("Баллы по физике 3");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
